acd094837b341eb0a1f9de5c815538b909ebdf55
stratijia <stratijia@gmail.com>
2023-08-31 18:23:33 +0100

    vv

--- index.js
+++ index.js
@@ -31,5 +31,6 @@ http
  * Exercise:
  * - Read “index.html.template” file
  *  - On the request, read user browser information such as device (let’s check if user is mobile, tablet or desktop) and language (in en-gb form) (don’t tell the candidate to use headers, correct ones are “User-Agent” and “Accept-Language”, candidate can use any library to parse “User-Agent”)
+ * https://lkrqjw-8080.csb.app/
  *  - Return read template file with replaced “{{device}}” with user device info and “{{language}}” with user preferred language
  */
